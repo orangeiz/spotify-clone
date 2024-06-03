492981f17abc47a0f1ce3ca90fd998d0a7dcb538
Orange <orangegamerz2124@gmail.com>
2024-06-03 18:12:40 +0530

    hm

--- hooks/useOnPlay.ts
+++ hooks/useOnPlay.ts
@@ -2,20 +2,16 @@ import { Song } from "@/types";
 import usePlayer from "./usePlayer";
 import useAuthModel from "./useAuthModal";
 import { useUser } from "./useUser";
-import useSubscribeModal from "./useSubscribeModal";
 
 const useOnPlay=(songs:Song[])=>{
     const player=usePlayer();
-    const subscribeModal=useSubscribeModal();
     const authModal=useAuthModel();
-    const {user,subscription}=useUser();
+    const {user}=useUser();
     const onPlay=(id:string)=>{
         if(!user){
             return authModal.onOpen();
         }
-        if(!subscription){
-            return subscribeModal.onOpen();
-        }
+       
         player.setId(id);
         player.setIds(songs.map((song)=>song.id));
     };
